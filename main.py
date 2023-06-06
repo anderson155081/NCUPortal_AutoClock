5ff7579c45a38194f254a50185557abf075d7d44
AndersonChenAn <anderson155081@gmail.com>
2023-06-06 21:06:58 +0800

    remove pypasser

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from pypasser import reCaptchaV2
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 import schedule
 from time import sleep
 import logging
 import datetime
 import os
 from dotenv import load_dotenv
-from get_work import get_entries_from_json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 from line_notification import send_line_notification
+from get_work import get_entries_from_json
 
 logging.basicConfig(
     format='%(asctime)s - %(message)s',
@@ -55,26 +55,24 @@ def signInOut(job_code, is_signin, run_date, message):
 
             username = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="inputAccount"]')))
             password = driver.find_element(By.XPATH, '//*[@id="inputPassword"]')
-            # change this for chinese
+            
+            # change this if your system language is chinese
             #submit = driver.find_element(By.XPATH, '/html/body/section/div/div/div[1]/div/div/form/fieldset/div[4]/button')
             submit = driver.find_element(By.XPATH, '/html/body/section/div/div/div[1]/div/div/form/fieldset/div[3]/button')
 
             username.send_keys(os.getenv("USERNAME"))
             password.send_keys(os.getenv("PASSWORD"))
 
-            # if need to bypass reCaptchaV2
-            # is_checked = reCaptchaV2(driver)
-
-            # if is_checked:
-            #     print("byPass Success")
             submit.click()
             sleep(5)
             driver.get("https://cis.ncu.edu.tw/HumanSys/")
             login_inner = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="head-meunu"]/div/div/nav/ul/li/a')))
             login_inner.click()
-            # change this for chinese
+
+            # change this if your system language is chinese
             #login_connect = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/form/fieldset/div[3]/div/div/div/button')
             login_connect = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[2]/div/div/div/form/fieldset/div[3]/div/div/div/button')))
+            
             login_connect.click()
             sleep(5)
             driver.get("https://cis.ncu.edu.tw/HumanSys/student/stdSignIn/create?ParttimeUsuallyId="+job_code)
@@ -95,6 +93,8 @@ def signInOut(job_code, is_signin, run_date, message):
                 driver.get_screenshot_as_file("signin.png")
                 logging.info("Sign In Success")
                 send_line_notification(f"{job_code} : Sign In Success", "signin.png")
+            
+            driver.quit()
 
         except Exception as e:
             logging.error(e)
@@ -109,9 +109,11 @@ def jobs():
     for entry in entries:
         schedule.every().day.at(entry["start_time"]).do(lambda: signInOut(entry["job_code"],False, entry["run_date"], entry["message"]))
         schedule.every().day.at(entry["end_time"]).do(lambda: signInOut(entry["job_code"],True, entry["run_date"], entry["message"]))
+
         logging.info("Job Scheduled.")
         logging.info("Job Code: " + entry["job_code"]+ " Start Time: " + \
                      entry["start_time"] + " End Time: " + entry["end_time"] + " Run Date: " + entry["run_date"])
+        
         send_line_notification("Job Code: " + entry["job_code"]+ "\nStart Time: " + \
                         entry["start_time"] + "\nEnd Time: " + entry["end_time"] + "\nRun Date: " + entry["run_date"])
 
